d5a9c974b7d1d0da2cd017bb2c256c03f4febeec
.Vibhuu <144808765+VVUx21@users.noreply.github.com>
2025-09-06 20:48:00 +0530

    Disable user redirection in RootLayout
    Comment out user authentication logic.

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+
 import { getLoggedInUser } from "@/lib/server/users.actions";
 import { redirect } from "next/navigation";
 
@@ -6,8 +7,8 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user1 = await getLoggedInUser();
-  if (!user1) redirect("/Sign-in");
+  //const user1 = await getLoggedInUser();
+  //if (!user1) redirect("/Sign-in");
   return (
     <main className="flex h-screen w-full font-inter">
       <div className="flex size-full flex-col ">
